7aaab90a8b251fec32375627f4998fcfe2a38192
kevinsthompson <github@purple-om.com>
2025-10-30 20:23:44 +1000

    .

---
+++ play_from_scracth.py
@@ -0,0 +1,13 @@
+# %%
+{
+    "cells": [],
+    "metadata": {},
+    "nbformat": 4,
+    "nbformat_minor": 5
+}
+
+# %%
+
+
+
+# "jupyter": "This is a Jupyter notebook.  Each cell is delimited by # %%.  Please can you start each cell with an enumerated cell number like # %% [Cell 1], # %% [Cell 2], etc. And please provide some debugging output as each cell starts to run and complete. Finally, please ensure that any code you provide is valid Python code that can run in a Jupyter notebook environment.:"   
